291fdcc32e9da8df6d16754a15e30929a2200ad9
kiku ishimoto <kikuish@gmail.com>
2020-05-06 18:52:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  if array.include('cheddar') || array.include('gouda') || array.include('camembert')
+  if array.include?('cheddar') || array.include?('gouda') || array.include?('camembert')
     true
   else
     nil

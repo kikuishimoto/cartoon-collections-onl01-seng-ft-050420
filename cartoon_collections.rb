d1b154fbdaf83146ac2cef0229995ef6b161e74b
kiku ishimoto <kikuish@gmail.com>
2020-05-06 18:33:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,12 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  index = 0
+  array.each_with_index { |item, index| puts "#{index + 1} : #{item}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
+
+  array.map{ |x| x.capitalize + '!'}
+
   # Your code here
 end
 
